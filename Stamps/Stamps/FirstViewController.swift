897b170bd005388dabbe5f9231a17f790bb3be93
Vladimir Svidersky <svidersky@gmail.com>
2020-01-17 21:19:55 -0800

    STM-1: initial project setup

---
+++ Stamps/Stamps/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Stamps
+//
+//  Created by Vladimir Svidersky on 1/17/20.
+//  Copyright © 2020 Vladimir Svidersky. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Stamps/Stamps/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Stamps
+//
+//  Created by Vladimir Svidersky on 1/17/20.
+//  Copyright © 2020 Vladimir Svidersky. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

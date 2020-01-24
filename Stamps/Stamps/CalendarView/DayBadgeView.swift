0820d956ea5ce08a94dd06c73f066c0175de8ac4
Vladimir Svidersky <svidersky@gmail.com>
2020-01-23 21:11:39 -0800

    STM-8: Fixed custom badge drawing logic to properly squish badges together when there are more then 4 of them

--- Stamps/Stamps/CalendarView/DayBadgeView.swift
+++ Stamps/Stamps/CalendarView/DayBadgeView.swift
@@ -14,17 +14,16 @@ class DayBadgeView : UIView {
     var badges: [UIColor]?
     
     let padding = 3.0
+    let margin = 8.0
     
-    //initWithFrame to init view from code
+    // initWithFrame to init view from code
     override init(frame: CGRect) {
-      super.init(frame: frame)
-      // setupView()
+        super.init(frame: frame)
     }
     
-    //initWithCode to init view from xib or storyboard
+    // initWithCode to init view from xib or storyboard
     required init?(coder aDecoder: NSCoder) {
-      super.init(coder: aDecoder)
-      // setupView()
+        super.init(coder: aDecoder)
     }
     
     override func draw(_ rect: CGRect) {
@@ -32,18 +31,25 @@ class DayBadgeView : UIView {
             return
         }
 
-        let size = Double(rect.height)
+        let size = Double(rect.height) // this will be badge circle diameter
+
         // Center all badges
-        let leftMargin = (Double(rect.width) - (size + padding) * Double(badges.count)) / 2 + padding / 2 
+        // First try to calculate margin if we use default padding between them
+        var currentPadding = padding
+        var leftMargin = (Double(rect.width) - (size + padding) * Double(badges.count)) / 2 + padding / 2
+        
+        // If margin appear smaller then default one - decrease padding so badges will be sqwished together
+        if leftMargin < margin {
+            leftMargin = margin
+            currentPadding = (Double(rect.width) - (size * Double(badges.count)) - margin * 2) / (Double(badges.count) - 1)
+        }
+        
         for i in 0..<badges.count {
-            let badgeRect = CGRect(x: leftMargin + (size + padding) * Double(i), y: 0, width: size, height: size)
+            let badgeRect = CGRect(x: leftMargin + (size + currentPadding) * Double(i), y: 0, width: size, height: size)
             let bpath = UIBezierPath(ovalIn: badgeRect)
             UIColor.clear.setFill()
             badges[i].set()
             bpath.fill()
-            
-//            UIColor.white.setStroke()
-//            bpath.stroke()
         }
     }
 }

b586ff4598c2af22ae63a7efd3edffa0a0b4ee25
Vladimir Svidersky <svidersky@gmail.com>
2021-03-14 14:43:33 -0700

    Cleanup

--- Emojical/Emojical/Application Pages/Onboarding/Implementation/WelcomeViewController.swift
+++ Emojical/Emojical/Application Pages/Onboarding/Implementation/WelcomeViewController.swift
@@ -21,17 +21,9 @@ class WelcomeViewController : UIViewController, WelcomeView {
     @IBOutlet weak var text2Label: UILabel?
     @IBOutlet weak var text2Bubble: UIView?
 
-    @IBOutlet weak var focusView: UIView?
-    @IBOutlet weak var focusViewLeadingConstraint: NSLayoutConstraint!
-
-
     // MARK: - DI
 
     var presenter: WelcomePresenterProtocol!
-    
-    // var gap: Float = 10.0
-
-    // MARK: - State
     
     // MARK: - View lifecycle
     
@@ -52,7 +44,7 @@ class WelcomeViewController : UIViewController, WelcomeView {
         super.viewDidAppear(animated)
     }
     
-    // MARK: - CongratsView
+    // MARK: - WelcomeView
     
     /// Dismiss button tapped
     var onDismiss: (() -> Void)? 
@@ -82,42 +74,28 @@ class WelcomeViewController : UIViewController, WelcomeView {
     // MARK: - Private helpers
     
     private func configureViews() {
-        
         view.backgroundColor = UIColor.systemBackground.withAlphaComponent(0.3)
-        
-        dismissButton.layer.cornerRadius = 15.0
+
+        dismissButton.layer.cornerRadius = Specs.textBubbleRadius
         dismissButton.layer.shadowOffset = Specs.shadowOffset
         dismissButton.layer.shadowOpacity = Specs.shadowOpacity
         dismissButton.layer.shadowRadius = Specs.shadowRadius
         
         text1Label.font = Theme.main.fonts.listBody
-        text2Label?.font = Theme.main.fonts.listBody
-
         text1Bubble.backgroundColor = Theme.main.colors.tint.withAlphaComponent(0.2)
-        text1Bubble.layer.cornerRadius = 15.0
+        text1Bubble.layer.cornerRadius = Specs.textBubbleRadius
+
+        text2Label?.font = Theme.main.fonts.listBody
         text2Bubble?.backgroundColor = Theme.main.colors.tint.withAlphaComponent(0.2)
-        text2Bubble?.layer.cornerRadius = 15.0
-        
-        focusView?.backgroundColor = UIColor.clear
-        focusView?.layer.borderWidth = 5.0
-        focusView?.layer.borderColor = Theme.main.colors.tint.cgColor
-        focusView?.layer.cornerRadius = (focusView?.bounds.width ?? 0) / 2.0
-        
-        focusViewLeadingConstraint?.constant = view.bounds.width / 4 * 1.5
+        text2Bubble?.layer.cornerRadius = Specs.textBubbleRadius
     }
 }
 
 // MARK: - Specs
 fileprivate struct Specs {
     
-    /// Bottom constraint for the initial modal view position
-    static let bottomButtonsMargin: CGFloat = 160.0
-    
-    /// Panning threshold after which view will be dismissed
-    static let dismissThreshold: CGFloat = 250.0
-    
     /// Modal corner radius
-    static let modalCornerRadius: CGFloat = 20.0
+    static let textBubbleRadius: CGFloat = 15.0
     
     /// Shadow radius
     static let shadowRadius: CGFloat = 8.0
@@ -127,16 +105,4 @@ fileprivate struct Specs {
     
     /// Shadow offset
     static let shadowOffset = CGSize.zero
-
-    /// Emoji font size for award icon
-    static let emojiFontSize: CGFloat = 48.0
-    
-    struct animation {
-
-        /// Emoji font size for award icon
-        static let progressDuration: TimeInterval = 1.0
-
-        /// Emoji font size for award icon
-        static let transitionToAwardDuration: TimeInterval = 0.5
-    }
 }
